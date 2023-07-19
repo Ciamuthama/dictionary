3ec8129140fcc078fe7f25e13e8f34bcb4c5be84
Ciamuthama <muthamapeter79@gmail.com>
2023-07-19 14:17:09 +0300

    dictionary CLI application

---
+++ index.js
@@ -0,0 +1,35 @@
+const readline = require("readline");
+
+// Function to fetch word definitions from a dictionary API
+function getWordDefinition(word) {
+  const request = require("request");
+  request.get(
+    {
+      url: "https://api.api-ninjas.com/v1/dictionary?word=" + word,
+      headers: {
+        "X-Api-Key": "NKfaedglac+/dnpKNKCA4w==qzmQlEDCP4rn0quO",
+      },
+    },
+    function (error, response, body) {
+      if (error) return console.error("Request failed:", error);
+      else if (response.statusCode !== 200)
+        return console.error(
+          "Error:",
+          response.statusCode,
+          body.toString("utf8")
+        );
+      else console.log(body);
+    }
+  );
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Enter a word to get its definition: ", (word) => {
+  const definition = getWordDefinition(word);
+  console.log(definition);
+  rl.close();
+});
